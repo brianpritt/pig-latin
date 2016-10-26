1d300dd2388a99ad5798f2b5a4eafdb2ba292770
Brian Pritt and Luke Philips <brian@>
2016-10-26 14:22:03 -0700

    finish function altering words starting with a single consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,42 @@
-var input = "pear";
 var vowels = ["a","e","i","o","u"];
 
 var translator = function(input){
   inputArray = input.split("");
   var ayWord;
-  vowels.forEach(function(vowel) {
-    if (inputArray[0] === vowel) {//handles single letter words and words that start with vowels
-     ayWord = input + "ay";
-   };//end if
- });//end vowel forEach
+  var trucatedArray;
+
+  if (inputArray[0] === "a"||inputArray[0] ===  "e" ||inputArray[0] === "i" ||inputArray[0] === "o" ||inputArray[0] === "u") {//handles single letter words and words that start with vowels
+      ayWord = input + "ay";
+   } else if{ 
+
+
+
+
+
+
+
+
+
+
+
+
+   }else  { //ends if for starting with vowel begin words beginning consonants //last arguement
+
+     firstLetter = inputArray.splice(0,1).toString();
+     ayWord = inputArray.join("") + firstLetter + "ay";
+
+
+  }//end if
   return ayWord;
 };//end translator
-
-
-var translatedString = translator(input);
-console.log(translatedString);
-
-
 
 
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-
     var inputString = $("#user-input").val();
-
-    var translatedString = tranlator(inputString);
+    var translatedString = translator(inputString);
 
     $("#translated-output h4").text(translatedString);
   });
